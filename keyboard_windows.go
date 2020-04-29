eiannone/keyboard

commit 453839b3c3abc109d4ad739ef92ecb854146ccff

Better management of releaseConsole() In some cases the inputEventsProducer() couldn't correctly detect a close request

keyboard_windows.go
@@ -1,284 +1,289 @@
 package keyboard
 
 import (
-    "syscall"
-    "unsafe"
+	"syscall"
+	"unsafe"
 
-    "golang.org/x/sys/windows"
+	"golang.org/x/sys/windows"
 )
 
 const (
-    vk_backspace   = 0x8
-    vk_tab         = 0x9
-    vk_enter       = 0xd
-    vk_esc         = 0x1b
-    vk_space       = 0x20
-    vk_pgup        = 0x21
-    vk_pgdn        = 0x22
-    vk_end         = 0x23
-    vk_home        = 0x24
-    vk_arrow_left  = 0x25
-    vk_arrow_up    = 0x26
-    vk_arrow_right = 0x27
-    vk_arrow_down  = 0x28
-    vk_insert      = 0x2d
-    vk_delete      = 0x2e
+	vk_backspace   = 0x8
+	vk_tab         = 0x9
+	vk_enter       = 0xd
+	vk_esc         = 0x1b
+	vk_space       = 0x20
+	vk_pgup        = 0x21
+	vk_pgdn        = 0x22
+	vk_end         = 0x23
+	vk_home        = 0x24
+	vk_arrow_left  = 0x25
+	vk_arrow_up    = 0x26
+	vk_arrow_right = 0x27
+	vk_arrow_down  = 0x28
+	vk_insert      = 0x2d
+	vk_delete      = 0x2e
 
-    vk_f1          = 0x70
-    vk_f2          = 0x71
-    vk_f3          = 0x72
-    vk_f4          = 0x73
-    vk_f5          = 0x74
-    vk_f6          = 0x75
-    vk_f7          = 0x76
-    vk_f8          = 0x77
-    vk_f9          = 0x78
-    vk_f10         = 0x79
-    vk_f11         = 0x7a
-    vk_f12         = 0x7b
+	vk_f1  = 0x70
+	vk_f2  = 0x71
+	vk_f3  = 0x72
+	vk_f4  = 0x73
+	vk_f5  = 0x74
+	vk_f6  = 0x75
+	vk_f7  = 0x76
+	vk_f8  = 0x77
+	vk_f9  = 0x78
+	vk_f10 = 0x79
+	vk_f11 = 0x7a
+	vk_f12 = 0x7b
 
-    right_alt_pressed  = 0x1
-    left_alt_pressed   = 0x2
-    right_ctrl_pressed = 0x4
-    left_ctrl_pressed  = 0x8
-    shift_pressed      = 0x10
+	right_alt_pressed  = 0x1
+	left_alt_pressed   = 0x2
+	right_ctrl_pressed = 0x4
+	left_ctrl_pressed  = 0x8
+	shift_pressed      = 0x10
 
-    k32_keyEvent = 0x1
+	k32_keyEvent = 0x1
 )
 
-
 type (
-    wchar     uint16
-    dword     uint32
-    word      uint16
+	wchar uint16
+	dword uint32
+	word  uint16
 
-    k32_event struct {
-        key_down          int32
-        repeat_count      word
-        virtual_key_code  word
-        virtual_scan_code word
-        unicode_char      wchar
-        control_key_state dword
-    }
+	k32_event struct {
+		key_down          int32
+		repeat_count      word
+		virtual_key_code  word
+		virtual_scan_code word
+		unicode_char      wchar
+		control_key_state dword
+	}
 )
 
 var (
-    kernel32 = syscall.NewLazyDLL("kernel32.dll")
+	kernel32 = syscall.NewLazyDLL("kernel32.dll")
 
-    k32_WaitForMultipleObjects = kernel32.NewProc("WaitForMultipleObjects")
-    k32_ReadConsoleInputW      = kernel32.NewProc("ReadConsoleInputW")
+	k32_WaitForMultipleObjects = kernel32.NewProc("WaitForMultipleObjects")
+	k32_ReadConsoleInputW      = kernel32.NewProc("ReadConsoleInputW")
 
-    hConsoleIn syscall.Handle
-    hInterrupt windows.Handle
+	hConsoleIn syscall.Handle
+	hInterrupt windows.Handle
 
-    cancel_comm      = make(chan bool, 1)
-    cancel_done_comm = make(chan bool)
+	cancel_comm      = make(chan bool, 1)
+	cancel_done_comm = make(chan bool, 1)
 
-    // This is just to prevent heap allocs at all costs
-    tmpArg dword
+	// This is just to prevent heap allocs at all costs
+	tmpArg dword
 )
 
 func getError(errno syscall.Errno) error {
-    if errno != 0 {
-        return error(errno)
-    } else {
-        return syscall.EINVAL
-    }
+	if errno != 0 {
+		return error(errno)
+	} else {
+		return syscall.EINVAL
+	}
 }
 
 func getKeyEvent(r *k32_event) (keyEvent, bool) {
-    e := keyEvent{}
+	e := keyEvent{}
 
-    if r.key_down == 0 {
-        return e, false
-    }
+	if r.key_down == 0 {
+		return e, false
+	}
 
-    ctrlPressed := r.control_key_state&(left_ctrl_pressed|right_ctrl_pressed) != 0
+	ctrlPressed := r.control_key_state&(left_ctrl_pressed|right_ctrl_pressed) != 0
 
-    if r.virtual_key_code >= vk_f1 && r.virtual_key_code <= vk_f12 {
-        switch r.virtual_key_code {
-        case vk_f1:
-            e.key = KeyF1
-        case vk_f2:
-            e.key = KeyF2
-        case vk_f3:
-            e.key = KeyF3
-        case vk_f4:
-            e.key = KeyF4
-        case vk_f5:
-            e.key = KeyF5
-        case vk_f6:
-            e.key = KeyF6
-        case vk_f7:
-            e.key = KeyF7
-        case vk_f8:
-            e.key = KeyF8
-        case vk_f9:
-            e.key = KeyF9
-        case vk_f10:
-            e.key = KeyF10
-        case vk_f11:
-            e.key = KeyF11
-        case vk_f12:
-            e.key = KeyF12
-        default:
-            panic("unreachable")
-        }
+	if r.virtual_key_code >= vk_f1 && r.virtual_key_code <= vk_f12 {
+		switch r.virtual_key_code {
+		case vk_f1:
+			e.key = KeyF1
+		case vk_f2:
+			e.key = KeyF2
+		case vk_f3:
+			e.key = KeyF3
+		case vk_f4:
+			e.key = KeyF4
+		case vk_f5:
+			e.key = KeyF5
+		case vk_f6:
+			e.key = KeyF6
+		case vk_f7:
+			e.key = KeyF7
+		case vk_f8:
+			e.key = KeyF8
+		case vk_f9:
+			e.key = KeyF9
+		case vk_f10:
+			e.key = KeyF10
+		case vk_f11:
+			e.key = KeyF11
+		case vk_f12:
+			e.key = KeyF12
+		default:
+			panic("unreachable")
+		}
 
-        return e, true
-    }
+		return e, true
+	}
 
-    if r.virtual_key_code <= vk_delete {
-        switch r.virtual_key_code {
-        case vk_insert:
-            e.key = KeyInsert
-        case vk_delete:
-            e.key = KeyDelete
-        case vk_home:
-            e.key = KeyHome
-        case vk_end:
-            e.key = KeyEnd
-        case vk_pgup:
-            e.key = KeyPgup
-        case vk_pgdn:
-            e.key = KeyPgdn
-        case vk_arrow_up:
-            e.key = KeyArrowUp
-        case vk_arrow_down:
-            e.key = KeyArrowDown
-        case vk_arrow_left:
-            e.key = KeyArrowLeft
-        case vk_arrow_right:
-            e.key = KeyArrowRight
-        case vk_backspace:
-            if ctrlPressed {
-                e.key = KeyBackspace2
-            } else {
-                e.key = KeyBackspace
-            }
-        case vk_tab:
-            e.key = KeyTab
-        case vk_enter:
-            e.key = KeyEnter
-        case vk_esc:
-            e.key = KeyEsc
-        case vk_space:
-            if ctrlPressed {
-                // manual return here, because KeyCtrlSpace is zero
-                e.key = KeyCtrlSpace
-                return e, true
-            } else {
-                e.key = KeySpace
-            }
-        }
+	if r.virtual_key_code <= vk_delete {
+		switch r.virtual_key_code {
+		case vk_insert:
+			e.key = KeyInsert
+		case vk_delete:
+			e.key = KeyDelete
+		case vk_home:
+			e.key = KeyHome
+		case vk_end:
+			e.key = KeyEnd
+		case vk_pgup:
+			e.key = KeyPgup
+		case vk_pgdn:
+			e.key = KeyPgdn
+		case vk_arrow_up:
+			e.key = KeyArrowUp
+		case vk_arrow_down:
+			e.key = KeyArrowDown
+		case vk_arrow_left:
+			e.key = KeyArrowLeft
+		case vk_arrow_right:
+			e.key = KeyArrowRight
+		case vk_backspace:
+			if ctrlPressed {
+				e.key = KeyBackspace2
+			} else {
+				e.key = KeyBackspace
+			}
+		case vk_tab:
+			e.key = KeyTab
+		case vk_enter:
+			e.key = KeyEnter
+		case vk_esc:
+			e.key = KeyEsc
+		case vk_space:
+			if ctrlPressed {
+				// manual return here, because KeyCtrlSpace is zero
+				e.key = KeyCtrlSpace
+				return e, true
+			} else {
+				e.key = KeySpace
+			}
+		}
 
-        if e.key != 0 {
-            return e, true
-        }
-    }
+		if e.key != 0 {
+			return e, true
+		}
+	}
 
-    if ctrlPressed {
-        if Key(r.unicode_char) >= KeyCtrlA && Key(r.unicode_char) <= KeyCtrlRsqBracket {
-            e.key = Key(r.unicode_char)
-            return e, true
-        }
-        switch r.virtual_key_code {
-        case 192, 50:
-            // manual return here, because KeyCtrl2 is zero
-            e.key = KeyCtrl2
-            return e, true
-        case 51:
-            e.key = KeyCtrl3
-        case 52:
-            e.key = KeyCtrl4
-        case 53:
-            e.key = KeyCtrl5
-        case 54:
-            e.key = KeyCtrl6
-        case 189, 191, 55:
-            e.key = KeyCtrl7
-        case 8, 56:
-            e.key = KeyCtrl8
-        }
+	if ctrlPressed {
+		if Key(r.unicode_char) >= KeyCtrlA && Key(r.unicode_char) <= KeyCtrlRsqBracket {
+			e.key = Key(r.unicode_char)
+			return e, true
+		}
+		switch r.virtual_key_code {
+		case 192, 50:
+			// manual return here, because KeyCtrl2 is zero
+			e.key = KeyCtrl2
+			return e, true
+		case 51:
+			e.key = KeyCtrl3
+		case 52:
+			e.key = KeyCtrl4
+		case 53:
+			e.key = KeyCtrl5
+		case 54:
+			e.key = KeyCtrl6
+		case 189, 191, 55:
+			e.key = KeyCtrl7
+		case 8, 56:
+			e.key = KeyCtrl8
+		}
 
-        if e.key != 0 {
-            return e, true
-        }
-    }
+		if e.key != 0 {
+			return e, true
+		}
+	}
 
-    if r.unicode_char != 0 {
-        e.rune = rune(r.unicode_char)
-        return e, true
-    }
+	if r.unicode_char != 0 {
+		e.rune = rune(r.unicode_char)
+		return e, true
+	}
 
-    return e, false
+	return e, false
+}
+
+func produceEvent(event keyEvent) bool {
+	select {
+	case input_comm <- event:
+		return true
+	case <-cancel_comm:
+		cancel_done_comm <- true
+		return false
+	}
 }
 
 func inputEventsProducer() {
-    var input [20]uint16
-    for {
-        // Wait for events
-        r0, _, e1 := syscall.Syscall6(k32_WaitForMultipleObjects.Addr(), 4,
-            uintptr(2), uintptr(unsafe.Pointer(&hConsoleIn)), 0, windows.INFINITE, 0, 0)
-        if uint32(r0) == windows.WAIT_FAILED {
-            input_comm <- keyEvent{err: getError(e1)}
-        }
+	var input [20]uint16
+	for {
+		// Wait for events
+		r0, _, e1 := syscall.Syscall6(k32_WaitForMultipleObjects.Addr(), 4,
+			uintptr(2), uintptr(unsafe.Pointer(&hConsoleIn)), 0, windows.INFINITE, 0, 0)
+		if uint32(r0) == windows.WAIT_FAILED && false == produceEvent(keyEvent{err: getError(e1)}) {
+			return
+		}
 
-        select {
-        case <-cancel_comm:
-            cancel_done_comm <- true
-            return
-        default:
-        }
-
-        // Get console input
-        r0, _, e1 = syscall.Syscall6(k32_ReadConsoleInputW.Addr(), 4,
-            uintptr(hConsoleIn), uintptr(unsafe.Pointer(&input[0])), 1, uintptr(unsafe.Pointer(&tmpArg)), 0, 0)
-        if int(r0) == 0 {
-            input_comm <- keyEvent{err: getError(e1)}
-        }
-
-        if input[0] == k32_keyEvent {
-            kEvent := (*k32_event)(unsafe.Pointer(&input[2]))
-            ev, ok := getKeyEvent(kEvent)
-            if ok {
-                for i := 0; i < int(kEvent.repeat_count); i++ {
-                    select {
-                    case <-cancel_comm:
-                        cancel_done_comm <- true
-                        return
-                    case input_comm <- ev:
-                        break
-                    }
-                }
-            }
-        }
-    }
+		// Get console input
+		r0, _, e1 = syscall.Syscall6(k32_ReadConsoleInputW.Addr(), 4,
+			uintptr(hConsoleIn), uintptr(unsafe.Pointer(&input[0])), 1, uintptr(unsafe.Pointer(&tmpArg)), 0, 0)
+		if int(r0) == 0 {
+			if false == produceEvent(keyEvent{err: getError(e1)}) {
+				return
+			}
+		} else if input[0] == k32_keyEvent {
+			kEvent := (*k32_event)(unsafe.Pointer(&input[2]))
+			ev, ok := getKeyEvent(kEvent)
+			if ok {
+				for i := 0; i < int(kEvent.repeat_count); i++ {
+					if false == produceEvent(ev) {
+						return
+					}
+				}
+			}
+		} else {
+			select {
+			case <-cancel_comm:
+				cancel_done_comm <- true
+				return
+			default:
+			}
+		}
+	}
 }
 
 func initConsole() (err error) {
-    // Create an interrupt event
-    hInterrupt, err = windows.CreateEvent(nil, 0, 0, nil)
-    if err != nil {
-        return err
-    }
+	// Create an interrupt event
+	hInterrupt, err = windows.CreateEvent(nil, 0, 0, nil)
+	if err != nil {
+		return err
+	}
 
-    hConsoleIn, err = syscall.Open("CONIN$", windows.O_RDWR, 0)
-    if err != nil {
-        windows.Close(hInterrupt)
-        return
-    }
+	hConsoleIn, err = syscall.Open("CONIN$", windows.O_RDWR, 0)
+	if err != nil {
+		windows.Close(hInterrupt)
+		return
+	}
 
-    go inputEventsProducer()
-    return
+	go inputEventsProducer()
+	return
 }
 
 func releaseConsole() {
-    // Stop events producer
-    cancel_comm <- true
-    windows.SetEvent(hInterrupt)
-    <-cancel_done_comm
+	// Stop events producer
+	cancel_comm <- true
+	windows.SetEvent(hInterrupt)
+	<-cancel_done_comm
 
-    syscall.Close(hConsoleIn)
-    windows.Close(hInterrupt)
+	syscall.Close(hConsoleIn)
+	windows.Close(hInterrupt)
 }

keyboard.go
@@ -3,217 +3,224 @@
 package keyboard
 
 import (
-    "os"
-    "os/signal"
-    "syscall"
-    "unicode/utf8"
-    "strings"
-    "fmt"
-    "runtime"
-    "unsafe"
+	"fmt"
+	"os"
+	"os/signal"
+	"runtime"
+	"strings"
+	"syscall"
+	"unicode/utf8"
+	"unsafe"
 
-    "golang.org/x/sys/unix"
+	"golang.org/x/sys/unix"
 )
 
 type (
-    input_event struct {
-        data []byte
-        err  error
-    }
+	input_event struct {
+		data []byte
+		err  error
+	}
 )
 
 var (
-    out   *os.File
-    in    int
+	out *os.File
+	in  int
 
-    // term specific keys
-    keys  []string
+	// term specific keys
+	keys []string
 
-    // termbox inner state
-    orig_tios      unix.Termios
+	// termbox inner state
+	orig_tios unix.Termios
 
-    sigio      = make(chan os.Signal, 1)
-    quit       = make(chan int)
-    inbuf      = make([]byte, 0, 128)
-    input_buf  = make(chan input_event)
+	sigio     = make(chan os.Signal, 1)
+	quit      = make(chan int)
+	inbuf     = make([]byte, 0, 128)
+	input_buf = make(chan input_event)
 )
 
 func fcntl(cmd int, arg int) error {
-    _, _, e := syscall.Syscall(unix.SYS_FCNTL, uintptr(in), uintptr(cmd), uintptr(arg))
-    if e != 0 {
-        return e
-    }
+	_, _, e := syscall.Syscall(unix.SYS_FCNTL, uintptr(in), uintptr(cmd), uintptr(arg))
+	if e != 0 {
+		return e
+	}
 
-    return nil
+	return nil
 }
 
 func ioctl(cmd int, termios *unix.Termios) error {
-    r, _, e := syscall.Syscall(unix.SYS_IOCTL, out.Fd(), uintptr(cmd), uintptr(unsafe.Pointer(termios)))
-    if r != 0 {
-        return os.NewSyscallError("SYS_IOCTL", e)
-    }
-    return nil
+	r, _, e := syscall.Syscall(unix.SYS_IOCTL, out.Fd(), uintptr(cmd), uintptr(unsafe.Pointer(termios)))
+	if r != 0 {
+		return os.NewSyscallError("SYS_IOCTL", e)
+	}
+	return nil
 }
 
 func parse_escape_sequence(buf []byte) (size int, event keyEvent) {
-    bufstr := string(buf)
-    for i, key := range keys {
-        if strings.HasPrefix(bufstr, key) {
-            event.rune = 0
-            event.key = Key(0xFFFF - i)
-            size = len(key)
-            return
-        }
-    }
-    return 0, event
+	bufstr := string(buf)
+	for i, key := range keys {
+		if strings.HasPrefix(bufstr, key) {
+			event.rune = 0
+			event.key = Key(0xFFFF - i)
+			size = len(key)
+			return
+		}
+	}
+	return 0, event
 }
 
-func extract_event(inbuf []byte) int {
-    if len(inbuf) == 0 {
-        return 0
-    }
+func extract_event(inbuf []byte) (int, keyEvent) {
+	if len(inbuf) == 0 {
+		return 0, keyEvent{}
+	}
 
-    if inbuf[0] == '\033' {
-        // possible escape sequence
-        if size, event := parse_escape_sequence(inbuf); size != 0 {
-            input_comm <- event
-            return size
-        }
+	if inbuf[0] == '\033' {
+		// possible escape sequence
+		if size, event := parse_escape_sequence(inbuf); size != 0 {
+			return size, event
+		} else {
+			// it's not a recognized escape sequence, then return Esc
+			return len(inbuf), keyEvent{key: KeyEsc}
+		}
+	}
 
-        // it's not a recognized escape sequence, then return Esc
-        input_comm <- keyEvent{key: KeyEsc}
-        return len(inbuf)
-    }
+	// if we're here, this is not an escape sequence and not an alt sequence
+	// so, it's a FUNCTIONAL KEY or a UNICODE character
 
-    // if we're here, this is not an escape sequence and not an alt sequence
-    // so, it's a FUNCTIONAL KEY or a UNICODE character
+	// first of all check if it's a functional key
+	if Key(inbuf[0]) <= KeySpace || Key(inbuf[0]) == KeyBackspace2 {
+		return 1, keyEvent{key: Key(inbuf[0])}
+	}
 
-    // first of all check if it's a functional key
-    if Key(inbuf[0]) <= KeySpace || Key(inbuf[0]) == KeyBackspace2 {
-        input_comm <- keyEvent{key: Key(inbuf[0])}
-        return 1
-    }
+	// the only possible option is utf8 rune
+	if r, n := utf8.DecodeRune(inbuf); r != utf8.RuneError {
+		return n, keyEvent{rune: r}
+	}
 
-    // the only possible option is utf8 rune
-    if r, n := utf8.DecodeRune(inbuf); r != utf8.RuneError {
-        input_comm <- keyEvent{rune: r}
-        return n
-    }
+	return 0, keyEvent{}
+}
 
-    return 0
+func produceEvent(event keyEvent) {
+	select {
+	case input_comm <- event:
+		return
+	case <-quit:
+		return
+	}
 }
 
 // Wait for an event and return it. This is a blocking function call.
 func inputEventsProducer() {
-    // try to extract event from input buffer, return on success
-    size := extract_event(inbuf)
-    if size != 0 {
-        copy(inbuf, inbuf[size:])
-        inbuf = inbuf[:len(inbuf)-size]
-    }
+	// try to extract event from input buffer, return on success
+	size, event := extract_event(inbuf)
+	if size != 0 {
+		copy(inbuf, inbuf[size:])
+		inbuf = inbuf[:len(inbuf)-size]
+		produceEvent(event)
+	}
 
-    for {
-        select {
-        case ev := <-input_buf:
-            if ev.err != nil {
-                input_comm <- keyEvent{err: ev.err}
-                return
-            }
+	for {
+		select {
+		case ev := <-input_buf:
+			if ev.err != nil {
+				produceEvent(keyEvent{err: ev.err})
+				return
+			}
 
-            inbuf = append(inbuf, ev.data...)
-            size = extract_event(inbuf)
-            if size != 0 {
-                copy(inbuf, inbuf[size:])
-                inbuf = inbuf[:len(inbuf)-size]
-            }
-        case <-quit:
-            return
-        }
-    }
+			inbuf = append(inbuf, ev.data...)
+			size, event = extract_event(inbuf)
+			if size != 0 {
+				copy(inbuf, inbuf[size:])
+				inbuf = inbuf[:len(inbuf)-size]
+				produceEvent(event)
+			}
+		case <-quit:
+			return
+		}
+	}
 }
 
 func initConsole() (err error) {
-    out, err = os.OpenFile("/dev/tty", unix.O_WRONLY, 0)
-    if err != nil {
-        return
-    }
-    in, err = syscall.Open("/dev/tty", unix.O_RDONLY, 0)
-    if err != nil {
-        return
-    }
+	out, err = os.OpenFile("/dev/tty", unix.O_WRONLY, 0)
+	if err != nil {
+		return
+	}
+	in, err = syscall.Open("/dev/tty", unix.O_RDONLY, 0)
+	if err != nil {
+		return
+	}
 
-    err = setup_term()
-    if err != nil {
-        return fmt.Errorf("Error while reading terminfo data: %v", err)
-    }
+	err = setup_term()
+	if err != nil {
+		return fmt.Errorf("Error while reading terminfo data: %v", err)
+	}
 
-    signal.Notify(sigio, unix.SIGIO)
+	signal.Notify(sigio, unix.SIGIO)
 
-    err = fcntl(unix.F_SETFL, unix.O_ASYNC|unix.O_NONBLOCK)
-    if err != nil {
-        return
-    }
-    err = fcntl(unix.F_SETOWN, unix.Getpid())
-    if runtime.GOOS != "darwin" && err != nil {
-        return
-    }
+	err = fcntl(unix.F_SETFL, unix.O_ASYNC|unix.O_NONBLOCK)
+	if err != nil {
+		return
+	}
+	err = fcntl(unix.F_SETOWN, unix.Getpid())
+	if runtime.GOOS != "darwin" && err != nil {
+		return
+	}
 
-    err = ioctl(ioctl_GETATTR, &orig_tios)
-    if err != nil {
-        return
-    }
+	err = ioctl(ioctl_GETATTR, &orig_tios)
+	if err != nil {
+		return
+	}
 
-    tios := orig_tios
-    tios.Iflag &^= unix.IGNBRK | unix.BRKINT | unix.PARMRK |
-        unix.ISTRIP | unix.INLCR | unix.IGNCR |
-        unix.ICRNL | unix.IXON
-    tios.Lflag &^= unix.ECHO | unix.ECHONL | unix.ICANON |
-        unix.ISIG | unix.IEXTEN
-    tios.Cflag &^= unix.CSIZE | unix.PARENB
-    tios.Cflag |= unix.CS8
-    tios.Cc[unix.VMIN] = 1
-    tios.Cc[unix.VTIME] = 0
+	tios := orig_tios
+	tios.Iflag &^= unix.IGNBRK | unix.BRKINT | unix.PARMRK |
+		unix.ISTRIP | unix.INLCR | unix.IGNCR |
+		unix.ICRNL | unix.IXON
+	tios.Lflag &^= unix.ECHO | unix.ECHONL | unix.ICANON |
+		unix.ISIG | unix.IEXTEN
+	tios.Cflag &^= unix.CSIZE | unix.PARENB
+	tios.Cflag |= unix.CS8
+	tios.Cc[unix.VMIN] = 1
+	tios.Cc[unix.VTIME] = 0
 
-    err = ioctl(ioctl_SETATTR, &tios)
-    if err != nil {
-        return err
-    }
+	err = ioctl(ioctl_SETATTR, &tios)
+	if err != nil {
+		return err
+	}
 
-    go func() {
-        buf := make([]byte, 128)
-        for {
-            select {
-            case <-sigio:
-                for {
-                    bytesRead, err := syscall.Read(in, buf)
-                    if err == unix.EAGAIN || err == unix.EWOULDBLOCK {
-                        break
-                    }
-                    if err != nil {
-                        bytesRead = 0
-                    }
-                    data := make([]byte, bytesRead)
-                    copy(data, buf)
-                    select {
-                    case input_buf <- input_event{data, err}:
-                        continue
-                    case <-quit:
-                        return
-                    }
-                }
-            case <-quit:
-                return
-            }
-        }
-    }()
+	go func() {
+		buf := make([]byte, 128)
+		for {
+			select {
+			case <-sigio:
+				for {
+					bytesRead, err := syscall.Read(in, buf)
+					if err == unix.EAGAIN || err == unix.EWOULDBLOCK {
+						break
+					}
+					if err != nil {
+						bytesRead = 0
+					}
+					data := make([]byte, bytesRead)
+					copy(data, buf)
+					select {
+					case input_buf <- input_event{data, err}:
+						continue
+					case <-quit:
+						return
+					}
+				}
+			case <-quit:
+				return
+			}
+		}
+	}()
 
-    go inputEventsProducer()
-    return
+	go inputEventsProducer()
+	return
 }
 
 func releaseConsole() {
-    quit <- 1
-    ioctl(ioctl_SETATTR, &orig_tios)
-    out.Close()
-    unix.Close(in)
+	quit <- 1
+	ioctl(ioctl_SETATTR, &orig_tios)
+	out.Close()
+	unix.Close(in)
 }

commit ff8b964fe1c95f3ffc963fa080d60db6bee3f473

Fixed regression introduced with 453839b3c3abc109d4ad739ef92ecb854146ccff

keyboard_windows.go
@@ -224,17 +224,17 @@ func produceEvent(event keyEvent) bool {
 func inputEventsProducer() {
 	var input [20]uint16
 	for {
-		select {
-		case <-quit:
-			return
-		default:
-		}
 		// Wait for events
 		// https://docs.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-waitformultipleobjects
 		r0, _, e1 := syscall.Syscall6(k32_WaitForMultipleObjects.Addr(), 4,
 			uintptr(2), uintptr(unsafe.Pointer(&hConsoleIn)), 0, windows.INFINITE, 0, 0)
 		if uint32(r0) == windows.WAIT_FAILED && false == produceEvent(keyEvent{err: getError(e1)}) {
 			return
+		}
+		select {
+		case <-quit:
+			return
+		default:
 		}
 
 		// Get console input